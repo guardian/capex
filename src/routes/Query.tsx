df9b985867139c2d5e224a4dc896570520c778ff
Paul Roberts <paul.roberts@guardian.co.uk>
2024-01-04 11:15:16 +0000

    cancel the fetch in effect cleanup func

--- src/routes/Query.tsx
+++ src/routes/Query.tsx
@@ -13,15 +13,6 @@ export interface QueryData<R> {
 
 type CapiResponse<T> = { response: T }
 
-async function submitQuery<ResponseT>(baseUrl: string, params: URLSearchParams, capiPath: string, apiKey: string): Promise<ResponseT> {
-  let paramsWithApiKey = new URLSearchParams(params)
-  paramsWithApiKey.set("api-key", apiKey)
-  let url = `${baseUrl}/${capiPath}?${paramsWithApiKey}`
-  let res = await fetch(url)
-  let body = await res.json() as CapiResponse<ResponseT>
-  return body.response
-}
-
 export function Query<Response extends { results: Result[] }, Result>({ initialQueryString, capiPath, resultRenderer, baseUrl, apiKey }: QueryData<Result>) {
   let [params, updateParamsState] = useState(new URLSearchParams(initialQueryString))
   let [results, updateResults] = useState<Result[]>([])
@@ -31,9 +22,27 @@ export function Query<Response extends { results: Result[] }, Result>({ initialQ
   }
 
   useEffect(() => {
+
+    // this abortcontroller stuff is how to signal to a fetch request to stop
+    const controller = new AbortController()
+    const signal = controller.signal
+
+    async function submitQuery<ResponseT>(): Promise<ResponseT> {
+      let paramsWithApiKey = new URLSearchParams(params)
+      paramsWithApiKey.set("api-key", apiKey)
+      let url = `${baseUrl}/${capiPath}?${paramsWithApiKey}`
+      let res = await fetch(url, { signal })
+      let body = await res.json() as CapiResponse<ResponseT>
+      return body.response
+    }
+
     console.log("submitting query")
-    let p = submitQuery<Response>(baseUrl, params, capiPath, apiKey).then(res => updateResults(res.results))
+    let p = submitQuery<Response>()
+      .then(res => updateResults(res.results), (reason) => {if(reason != "aborted") throw reason })
+
     console.log("submitted", p)
+    // this is the clean up function for the effect, returned to react
+    return () => { console.log("cleaning up fetch()"); controller.abort("aborted") }
   }, [params])
 
   return (
