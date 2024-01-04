572303e1b30e51fd6e2dfd56a17a18b0d4177a87
Paul Roberts <paul.roberts@guardian.co.uk>
2024-01-04 18:17:38 +0000

    rudimentary config form
    for setting the api key into local storage

--- src/routes/Config.tsx
+++ src/routes/Config.tsx
@@ -1,5 +1,21 @@
-import { Link } from "react-router-dom";
+import { TextInput } from "@guardian/source-react-components"
 
-export function Config() {
-  return <div>Config (placeholder) page. <Link to={"/"}>(back)</Link></div>
+import { ConfigContext } from "../context/Config"
+import { useContext } from "react"
+import { Link } from "react-router-dom"
+
+export default function Config() {
+
+  let config = useContext(ConfigContext)
+
+  const updateApiKey = (newKey: string) => {
+    window.localStorage.setItem("capex-api-key", newKey)
+  }
+
+  return (
+    <div>
+      <TextInput label="API key" width={30} defaultValue={config.apiKey} onChange={(ev) => updateApiKey(ev.target.value) }/>
+      <Link to={"/"}>Done</Link>
+    </div>
+  )
 }
