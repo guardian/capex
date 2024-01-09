7231f3f763382691824d4e523367093a04070fed
Paul Roberts <paul.roberts@guardian.co.uk>
2024-01-09 15:57:04 +0000

    use hash router

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
-import {createBrowserRouter, Navigate, redirect, RouterProvider} from "react-router-dom"
+import {createHashRouter, Navigate, redirect, RouterProvider} from "react-router-dom"
 import Config from './routes/Config'
 import SearchQuery from './routes/SearchQuery'
-import { readConfig } from './context/Config'
+import { ConfigContext, readConfig } from './context/Config'
 
 const commonQueryParams = {
   initialQueryString: window.location.search,
@@ -19,7 +19,7 @@ const requiresApiKey = async () => {
   return null
 }
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <Navigate to={defaultQuery + window.location.search} replace={true} />
@@ -37,6 +37,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ConfigContext.Provider value={readConfig()}>
+      <RouterProvider router={router} />
+    </ConfigContext.Provider>
   </React.StrictMode>,
 )
